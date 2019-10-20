8fa4e177e22959f608125b2f4c2459ef008ac16b
shawn3298317 <shawn3298317lin@gmail.com>
2019-10-19 22:42:48 -0400

    Parse get query

--- folly/views.py
+++ folly/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the folly index.")
+    category = request.GET.get("category") # 0: cluster_map 1: heat_map
+    time_start = request.GET.get("time_st")
+    time_end = request.GET.get("time_end")
+    center_lat = request.GET.get("c_lat")
+    center_long = request.GET.get("c_long")
+
+    return HttpResponse("Hello, world. You're at the folly index. The arguments are:\n"
+                        "category = %s\ntime_start = %s\ntime_end = %s\ncenter_latitude = %s center_longtitude = %s"\
+                        % (category, time_start, time_end, center_lat, center_long))
+
--- insight/settings.py
+++ insight/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'insight.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
